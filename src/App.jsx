1be2ef3022c8e777c2dfca135d08379cdfd64540
Makiko Yokoyama <115846240+trimakichan@users.noreply.github.com>
2024-04-23 17:17:23 -0700

    finished navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
+import "./layout.scss"
+import Navbar from "./components/navbar/Navbar"
+
 function App() {
   return (
-    <div>Hello World</div>
-  )
-}
+    <div className="layout">
+      <Navbar />
+      </div>
+  )}
 
 export default App
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,39 @@
+import {useState} from 'react';
+import './navbar.scss';
+
+
+const Navbar = () => {
+const [open, setOpen] = useState(false)
+
+  return (
+    <nav>
+      <div className="left">
+        <a href="/" className='logo'>
+          <img src="/logo.png" alt="" />
+          <span>LamaEstate</span>
+        </a>
+        <a href='/'>Home</a>
+        <a href='/'>About</a>
+        <a href='/'>Contact</a>
+        <a href='/'>Agents</a>
+      </div>
+      <div className="right">
+        <a href='/' className=''>Sign in</a>
+        <a href='/' className='register'>Sign up</a>
+        <div className="menuIcon" >
+          <img src='/menu.png' alt="" onClick={() => setOpen(!open)} />
+        </div>
+        <div className={open ? "menu active" : "menu"}>
+          <a href='/'>Home</a>
+          <a href='/'>About</a>
+          <a href='/'>Contact</a>
+          <a href='/'>Agents</a>
+          <a href='/'>Sign in</a>
+          <a href='/'>Sign out</a>
+        </div>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
