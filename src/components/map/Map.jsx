8a3fd2fcc0aa20ee0f79e6d9c2628f5fc3c45ae9
Makiko Yokoyama <115846240+trimakichan@users.noreply.github.com>
2024-04-27 12:53:26 -0700

    Created a Listing page

---
+++ src/components/map/Map.jsx
@@ -0,0 +1,22 @@
+import './map.scss';
+import { MapContainer, TileLayer } from 'react-leaflet';
+import "leaflet/dist/leaflet.css"
+import Pin from '../pin/Pin';
+
+
+const Map = ({items}) => {
+
+  return (
+      <MapContainer center={[51.505, -0.09]} zoom={7} scrollWheelZoom={false} className='map'>
+          <TileLayer
+              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          
+          {items.map(item => (<Pin key={item.id} item={item}/> ))}
+        
+      </MapContainer>
+  )
+}
+
+export default Map
